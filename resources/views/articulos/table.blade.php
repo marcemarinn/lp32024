a816a6febd9e398bc525cbe0e5de2615b77ec954
marcemarinn <113055169+marcemarinn@users.noreply.github.com>
2024-08-05 18:53:33 -0400

    feat: se agrega en la vista lista la imagen

--- resources/views/articulos/table.blade.php
+++ resources/views/articulos/table.blade.php
@@ -2,36 +2,47 @@
     <div class="table-responsive">
         <table class="table" id="articulos-table">
             <thead>
-            <tr>
-                <th>Codigo</th>
-                <th>Descripcion</th>
-                <th>Precio</th>
-                <th>Imagen</th>
-                <th>Iva</th>
-                <th colspan="3">Acciones</th>
-            </tr>
+                <tr>
+                    <th>#</th>
+                    <th>Producto</th>
+                    <th>Precio</th>
+                    <th>Imagen</th>
+                    <th>Marca</th>
+                    <th>Iva</th>
+                    <th colspan="3">Operaciones</th>
+                </tr>
             </thead>
             <tbody>
-            @foreach($articulos as $articulo)
-                <tr>
-                    <td>{{ $articulo->mar_cod }}</td>
-                    <td>{{ $articulo->art_descripcion }}</td>
-                    <td>{{ $articulo->art_precio }}</td>
-                    <td>{{ $articulo->art_imagen }}</td>
-                    <td>{{ $articulo->art_iva }}</td>
-                    <td  style="width: 120px">
-                        {!! Form::open(['route' => ['articulos.destroy', $articulo->id_articulo], 'method' => 'delete']) !!}
-                        <div class='btn-group'>
-                            <a href="{{ route('articulos.edit', [$articulo->id_articulo]) }}"
-                               class='btn btn-default btn-xs'>
-                                <i class="far fa-edit"></i>
-                            </a>
-                            {!! Form::button('<i class="far fa-trash-alt"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
-                        </div>
-                        {!! Form::close() !!}
-                    </td>
-                </tr>
-            @endforeach
+                @foreach ($articulos as $articulo)
+                    <tr>
+                        <td>{{ $articulo->id_articulo }}</td>
+                        <td>{{ $articulo->art_descripcion }}</td>
+                        <td>{{ number_format($articulo->art_precio, 0, ',', '.') }}</td>
+                        <td>
+                            @if (!empty($articulo->art_imagen))
+                                <img src="{{ asset('img/articulos/' . $articulo->art_imagen) }}"
+                                    alt="{!! $articulo->art_imagen !!}"  width="90px" height="90px">
+                            @endif
+                        </td>
+                        <td>{{ $articulo->mar_descrip }}</td>
+                        <td>{{ $articulo->art_iva }}</td>
+                        <td style="width: 120px">
+                            {!! Form::open(['route' => ['articulos.destroy', $articulo->id_articulo], 'method' => 'delete']) !!}
+                            <div class='btn-group'>
+                                <a href="{{ route('articulos.edit', [$articulo->id_articulo]) }}"
+                                    class='btn btn-default btn-xs'>
+                                    <i class="far fa-edit"></i>
+                                </a>
+                                {!! Form::button('<i class="far fa-trash-alt"></i>', [
+                                    'type' => 'submit',
+                                    'class' => 'btn btn-danger btn-xs',
+                                    'onclick' => "return confirm('Are you sure?')",
+                                ]) !!}
+                            </div>
+                            {!! Form::close() !!}
+                        </td>
+                    </tr>
+                @endforeach
             </tbody>
         </table>
     </div>
